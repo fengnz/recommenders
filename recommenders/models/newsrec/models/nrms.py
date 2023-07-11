78a5c0c7474cbdd517d1515288f7c7e1fbaa643e
YuFeng Deng <fengnz@gmail.com>
2023-07-12 01:16:07 +1200

    use fast former

--- recommenders/models/newsrec/models/nrms.py
+++ recommenders/models/newsrec/models/nrms.py
@@ -255,7 +255,7 @@ class NRMSModel(BaseModel):
 
         print('looks good')
 
-        useFastFormer = 0
+        useFastFormer = 2
 
         print('Use Fast Former: ')
         print(str(useFastFormer))
